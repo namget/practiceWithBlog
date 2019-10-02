5d5a1c1ba668b89e8365de218ae06e11ca7205bc
yeboykr <yeboykr@bodyfriend.co.kr>
2019-10-02 17:06:44 +0900

    stringformat 블로그 정리

---
+++ app/src/main/java/com/example/myapplication/formatting/StringFormmat.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.formatting;
+
+public class StringFormmat {
+//    res/value/strings.xml
+//    <string name="try_message">남은 도전횟수는 %d입니다.</string>
+//    Resources res = getResources();
+//    String text = String.format(res.getString(R.string.try_message), remainingCount);
+}
