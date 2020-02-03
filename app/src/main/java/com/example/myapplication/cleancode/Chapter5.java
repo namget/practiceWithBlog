b18af24d22af199328772efe948ab4b0c6f9a9ad
namget <yeboykr@gmail.com>
2020-02-03 22:28:25 +0900

    cleancode

---
+++ app/src/main/java/com/example/myapplication/cleancode/Chapter5.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.cleancode;
+
+//public class Chapter5 {
+//
+//    public class ReporterConfig {
+///**
+//* 리포터 리스너의 클래스 이름
+//*/
+//        private String m_className;
+///**
+//* 리포터 리스너의 속성
+//*/
+//        private List<Property> m_properties = new ArrayList<Property>();
+//        public void addProperty(Property property) {
+//            m_properties.add(property);
+//        }
+//
+//
+//}
+//
+//    public class ReporterConfig {
+//        private String m_className;
+//        private List<Property> m_properties = new ArrayList<Property>();
