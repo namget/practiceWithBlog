94ad740bfc699cc6eb37046384ea15af4bb86801
namget <yeboykr@gmail.com>
2020-02-05 21:00:38 +0900

    cleancode test

---
+++ app/src/main/java/com/example/myapplication/cleancode/Chapter7.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.cleancode;
+
+public class Chapter7 {
+
+//    public void sendShutDown() {
+//        try {
+//            tryToShutDown();
+//        } catch (DeviceShutDownError e) {
+//            logger.log(e);
+//            private void tryToShutDown () throws DeviceShutDownError {
+//                DeviceHandle handle = getHandle(DEVl);
+//                DeviceRecord record = retrieveDeviceRecord(handle);
+//                pauseDevice(handle);
+//                clearDeviceWorkQueue(handle);
+//                closeDevice(handle);
+//            }
+//        }
+//    }
+}
